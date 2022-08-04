24672f058ac44e9d97db729eb7196f00cedf02a9
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-08-04 11:57:06 +0300

    3 and 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import uuid
 from datetime import datetime
 
@@ -13,23 +14,24 @@ class BaseModel:
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k != "__class__":
-                    self.k = v
-            if type(kwargs['created_at']) and\
-                        type(kwargs['updated_at']) == str:
-                self.created_at = datetime.strptime(kwargs['created_at'], tf)
-                self.updated_at = datetime.strptime(kwargs['updated_at'], tf)
+                    setattr(self, k, v)
+            if type(kwargs["created_at"]) and\
+                        type(kwargs["updated_at"]) == str:
+                self.created_at = datetime.strptime(kwargs["created_at"], tf)
+                self.updated_at = datetime.strptime(kwargs["updated_at"], tf)
             else:
-                self.created_at = datetime.utcnow()
+                self.created_at = datetime.now()
                 self.updated_at = self.created_at
             self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow().strftime(tf)
+            self.created_at = datetime.now()
             self.updated_at = self.created_at
+
     def __str__(self):
         """Informal str rep"""
-        return ("[BaseModel] ({:s}) {}"\
-                .format(self.id, self.__dict__))
+        return ("[{:s}] ({:s}) {}"\
+                .format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """updates the public instance
@@ -43,5 +45,9 @@ class BaseModel:
         all keys/values of __dict__
         of the instance"""
         nw_dict = self.__dict__.copy()
+        if "created_at" in nw_dict:
+            nw_dict["created_at"] = nw_dict["created_at"].strftime(tf)
+        if "updated_at" in nw_dict:
+            nw_dict["updated_at"] = nw_dict["updated_at"].strftime(tf)
         nw_dict["__class__"] = self.__class__.__name__
         return nw_dict
