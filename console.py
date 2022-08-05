d91b0c0c82e5d26d5ed4ad77e31a19fee636f6ad
Owinnie <tillawinnie@gmail.com>
2022-08-05 01:35:32 -0700

    6. console

--- console.py
+++ console.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
+"""Program entry"""
 
+import cmd
+from models.base_model import BaseModel
+
+classes = {"BaseModel": BaseModel}
+
+
+class HBNBCommand(cmd.Cmd):
+    """The console class"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        exit()
+
+    def do_EOF(self, arg):
+        """Exit the program"""
+        print()
+        exit()
+
+    def do_help(self, arg):
+        """Display help docs"""
+        cmd.Cmd.do_help(self, arg)
+
+    def emptyline(self):
+        """Do nothing...Override cmds
+        emptyline method"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
