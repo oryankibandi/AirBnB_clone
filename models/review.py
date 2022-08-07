7a36b6214735a914a25e98efaae74a3614592122
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-08-07 10:01:11 +0300

    reviews

--- models/review.py
+++ models/review.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""The reviews model"""
 
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Reviews"""
+    place_id = ''
+    user_id = ''
+    text = ''
