37fa2a43124b770fb2d9f4e633ee2456f180d178
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-08-07 09:36:38 +0300

    User

--- models/user.py
+++ models/user.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+""" class User inheriting
+from BaseModel"""
 
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """class user, a subclass of BaseModel"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
